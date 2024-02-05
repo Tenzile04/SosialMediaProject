19e88a94e232d1e717dd28f34e407a5ec1a48271
Tenzile04 <tenzileabdiyeva2004@gmail.com>
2024-02-05 23:47:11 +0400

    MessagePage

--- src/SosialMediaProject.MVC/obj/Debug/net6.0/SosialMediaProject.MVC.AssemblyInfo.cs
+++ src/SosialMediaProject.MVC/obj/Debug/net6.0/SosialMediaProject.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SosialMediaProject.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a59311b5d6694a19767f63192152d7898273246")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bb7bfba74ccaa430b2c59a060fbc4d7e3142cbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("SosialMediaProject.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SosialMediaProject.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
