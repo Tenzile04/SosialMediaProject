22edac0e2aa97bfcef65992770c49fad91131420
Tenzile04 <tenzileabdiyeva2004@gmail.com>
2024-02-09 00:20:02 +0400

    viewcommmit

--- src/SosialMediaProject.Business/obj/Debug/net6.0/SosialMediaProject.Business.AssemblyInfo.cs
+++ src/SosialMediaProject.Business/obj/Debug/net6.0/SosialMediaProject.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SosialMediaProject.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9cf237360ce659727503294d9a821012d7c6958")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d6df20aa1ba6bd71d19b183c7a3d6d2cf33fb03")]
 [assembly: System.Reflection.AssemblyProductAttribute("SosialMediaProject.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SosialMediaProject.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
